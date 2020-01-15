dbfcb354990bb749210782305c8350cce6f7ac7e
filipecosta90 <filipecosta.90@gmail.com>
2020-01-15 23:32:17 +0000

    [fix] upgrade scrpits

--- benchmark/redisgraph/scripts/redisgraph_run_graph500.sh
+++ benchmark/redisgraph/scripts/redisgraph_run_graph500.sh
@@ -14,6 +14,7 @@ DATA_NODE_FILE_NAME=${DATASET}/graph500_22_out_noheader
 
 # How many queries would be run
 SLEEP_BETWEEN_RUNS=${SLEEP_BETWEEN_RUNS:-"60"}
+PREFIX=${PREFIX:-"master"}
 
 for DEPTH in 1 2 3 6; do
 for WORKERS in 1 22; do
@@ -22,7 +23,7 @@ for run in 1 2 3; do
 
     python3 kn.py --url ${DATABASE_HOST}:${DATABASE_PORT} -g graph500_22 -s ${DATA_NODE_FILE_NAME} \
     -c ${SEEDS} -d ${DEPTH} -p redisgraph -l ${NODE_FILENAME} \
-    --threads ${WORKERS} -i ${ITERATIONS_PER_QUERY} --stdout > ${PREFIX}_${WORKERS}_workers_${DEPTH}_depth_run_${run}
+    --threads ${WORKERS} -i ${ITERATIONS_PER_QUERY} --stdout  |   tee ${PREFIX}_${WORKERS}_workers_${DEPTH}_depth_run_${run}
 
     echo "Sleeping for ${SLEEP_BETWEEN_RUNS} seconds"
     sleep ${SLEEP_BETWEEN_RUNS}
