b472bb2c768f2bd9a12cf100128c62758ee8b1b1
filipecosta90 <filipecosta.90@gmail.com>
2020-01-15 23:29:06 +0000

    [fix] upgrade scrpits

--- benchmark/redisgraph/scripts/redisgraph_run_graph500.sh
+++ benchmark/redisgraph/scripts/redisgraph_run_graph500.sh
@@ -3,10 +3,6 @@
 DATASET_DIR=${DATASET_DIR:-"graph500_22"}
 ITERATIONS_PER_QUERY=${ITERATIONS_PER_QUERY:-10}
 SEEDS=${SEEDS:-10}
-DEPTH=${DEPTH:-1}
-
-# How many concurrent worker would run queries - match num of cores, or default to 8
-WORKERS=${WORKERS:-$(grep -c ^processor /proc/cpuinfo 2>/dev/null || echo 8)}
 
 EXE_DIR=${EXE_DIR:-$(dirname $0)}
 source ${EXE_DIR}/common.sh
@@ -15,7 +11,22 @@ NODE_FILENAME=${NODE_FILENAME:-"graph500_22_unique_node_out"}
 
 DATA_NODE_FILE_NAME=${DATASET}/graph500_22_out_noheader
 
-# 300 seeds, depth 1
-python3 kn.py --url ${DATABASE_HOST}:${DATABASE_PORT} -g graph500_22 -s ${DATA_NODE_FILE_NAME} \
+
+# How many queries would be run
+SLEEP_BETWEEN_RUNS=${SLEEP_BETWEEN_RUNS:-"60"}
+
+for DEPTH in 1 2 3 6; do
+for WORKERS in 1 22; do
+for run in 1 2 3; do
+    echo "Running depth ${DEPTH}. Workers ${WORKERS}. RUN $run"
+
+    python3 kn.py --url ${DATABASE_HOST}:${DATABASE_PORT} -g graph500_22 -s ${DATA_NODE_FILE_NAME} \
     -c ${SEEDS} -d ${DEPTH} -p redisgraph -l ${NODE_FILENAME} \
-    --threads ${WORKERS} -i ${ITERATIONS_PER_QUERY} --stdout || exit 1
+    --threads ${WORKERS} -i ${ITERATIONS_PER_QUERY} --stdout > ${PREFIX}_${WORKERS}_workers_${DEPTH}_depth_run_${run}
+
+    echo "Sleeping for ${SLEEP_BETWEEN_RUNS} seconds"
+    sleep ${SLEEP_BETWEEN_RUNS}
+done
+done
+done
+
