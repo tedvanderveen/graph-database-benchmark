8971432cbc5ded973f34670331a4f0d3b5b3e2e2
filipecosta90 <filipecosta.90@gmail.com>
2020-01-15 21:49:54 +0000

    [add] improving helper scripts

--- benchmark/redisgraph/scripts/redisgraph_load_graph500.sh
+++ benchmark/redisgraph/scripts/redisgraph_load_graph500.sh
@@ -6,12 +6,13 @@ EXE_DIR=${EXE_DIR:-$(dirname $0)}
 source ${EXE_DIR}/common.sh
 
 MAX_TOKEN_SIZE=${MAX_TOKEN_SIZE:-8}
+NODE_FILENAME=${NODE_FILENAME:-"graph500_22_unique_node_out"}
 
-redis-cli -h ${DATABASE_HOST} -p ${DATABASE_PORT} del "graph500_22" 
+redis-cli -h ${DATABASE_HOST} -p ${DATABASE_PORT} del ${KEYNAME} 
 
 # Run RedisGraph bulk import script
-python3  bulk_insert.py graph500_22 --max-token-size ${MAX_TOKEN_SIZE} -n ${DATASET}/graph500_22_unique_node_out -r ${DATASET}/graph500_22_out \
+python3  bulk_insert.py ${KEYNAME} --max-token-size ${MAX_TOKEN_SIZE} -n ${DATASET}/${NODE_FILENAME} -r ${DATASET}/graph500_22_out \
   --host ${DATABASE_HOST} --port ${DATABASE_PORT} || exit 1
 
 # Create index on node ID property
-redis-cli -h ${DATABASE_HOST} -p ${DATABASE_PORT} graph.query "graph500_22" "create index on :graph500_22_unique_node(id)"
+redis-cli -h ${DATABASE_HOST} -p ${DATABASE_PORT} graph.query ${KEYNAME} "create index on :${NODE_FILENAME}(id)"
