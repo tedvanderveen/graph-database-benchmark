7cdd78dcab11e2539fd00bfe5557ab106c7ecb9b
filipecosta90 <filipecosta.90@gmail.com>
2020-01-15 23:22:20 +0000

    [fix] upgrade scrpits

--- benchmark/redisgraph/scripts/run_graph500.sh
+++ benchmark/redisgraph/scripts/run_graph500.sh
@@ -2,18 +2,25 @@
 
 # How many queries would be run
 HOST=${MAX_QUERIES:-"localhost"}
+PREFIX=${PREFIX:-"master"}
+S=${S:-"100000"}
+I=${I:-"10"}
 
 # How many queries would be run
 SLEEP_BETWEEN_RUNS=${SLEEP_BETWEEN_RUNS:-"60"}
 
 for depth in 1 2 3 6; do
 for worker_num in 1 22; do
-    echo "Running depth ${depth}. Workers ${worker_num}"
+for run in 1 2 3; do
+    echo "Running depth ${depth}. Workers ${worker_num}. RUN $run"
 
-    #WORKERS=${worker_num} DATABASE_HOST=${HOST} BULK_DATA_DIR=/root/bulk_data DEPTH=${depth} SEEDS=100000 ITERATIONS_PER_QUERY=10 ./scripts/redisgraph_run_graph500.sh
+    WORKERS=${worker_num} DATABASE_HOST=${HOST} \
+    BULK_DATA_DIR=/root/bulk_data DEPTH=${depth} SEEDS=$S \
+    ITERATIONS_PER_QUERY=$I ./scripts/redisgraph_run_graph500.sh > ${PREFIX}_${worker_num}_workers_${depth}_depth_run_${run}
 
     echo "Sleeping for ${SLEEP_BETWEEN_RUNS} seconds"
     sleep ${SLEEP_BETWEEN_RUNS}
+done
 done
 done
 
