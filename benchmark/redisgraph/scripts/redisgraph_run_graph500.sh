8fef8ca050e44ea35849ea86e46ae3c2dfdabf00
filipecosta90 <filipecosta.90@gmail.com>
2020-01-15 23:50:56 +0000

    [fix] upgrade scrpits

--- benchmark/redisgraph/scripts/redisgraph_run_graph500.sh
+++ benchmark/redisgraph/scripts/redisgraph_run_graph500.sh
@@ -2,7 +2,7 @@
 
 DATASET_DIR=${DATASET_DIR:-"graph500_22"}
 ITERATIONS_PER_QUERY=${ITERATIONS_PER_QUERY:-10}
-SEEDS=${SEEDS:-10}
+SEEDS=${SEEDS:-100000}
 
 EXE_DIR=${EXE_DIR:-$(dirname $0)}
 source ${EXE_DIR}/common.sh
@@ -16,7 +16,7 @@ DATA_NODE_FILE_NAME=${DATASET}/graph500_22_out_noheader
 SLEEP_BETWEEN_RUNS=${SLEEP_BETWEEN_RUNS:-"60"}
 PREFIX=${PREFIX:-"master"}
 
-for DEPTH in 1 2 3 6; do
+for DEPTH in 1 2; do
 for WORKERS in 1 22; do
 for run in 1 2 3; do
     echo "Running depth ${DEPTH}. Workers ${WORKERS}. RUN $run"
@@ -31,3 +31,36 @@ done
 done
 done
 
+SEEDS=1000
+
+for DEPTH in 3; do
+for WORKERS in 1 22; do
+for run in 1 2 3; do
+    echo "Running depth ${DEPTH}. Workers ${WORKERS}. RUN $run"
+
+    python3 kn.py --url ${DATABASE_HOST}:${DATABASE_PORT} -g graph500_22 -s ${DATA_NODE_FILE_NAME} \
+    -c ${SEEDS} -d ${DEPTH} -p redisgraph -l ${NODE_FILENAME} \
+    --threads ${WORKERS} -i ${ITERATIONS_PER_QUERY} --stdout  |   tee ${PREFIX}_${WORKERS}_workers_${DEPTH}_depth_run_${run}
+
+    echo "Sleeping for ${SLEEP_BETWEEN_RUNS} seconds"
+    sleep ${SLEEP_BETWEEN_RUNS}
+done
+done
+done
+
+SEEDS=100
+
+for DEPTH in 3; do
+for WORKERS in 1 22; do
+for run in 1 2 3; do
+    echo "Running depth ${DEPTH}. Workers ${WORKERS}. RUN $run"
+
+    python3 kn.py --url ${DATABASE_HOST}:${DATABASE_PORT} -g graph500_22 -s ${DATA_NODE_FILE_NAME} \
+    -c ${SEEDS} -d ${DEPTH} -p redisgraph -l ${NODE_FILENAME} \
+    --threads ${WORKERS} -i ${ITERATIONS_PER_QUERY} --stdout  |   tee ${PREFIX}_${WORKERS}_workers_${DEPTH}_depth_run_${run}
+
+    echo "Sleeping for ${SLEEP_BETWEEN_RUNS} seconds"
+    sleep ${SLEEP_BETWEEN_RUNS}
+done
+done
+done
